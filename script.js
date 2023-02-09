3100dd56beee7c37c7ff7d2a819719e417f02e83
preet-ki <108222325+preet-ki@users.noreply.github.com>
2023-02-09 23:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+//your code herefun
+function  mincost() {
+	let arr=[];
+	for(let i=0;i<n;i++){
+		arr.push(arr[i]);
+	}
+	let res=0;
+	while(arr.length>1){
+		let first=arr.shift();
+		let second=arr.shift();
+		res+=first+second;
+		arr.push(first+second);
+	}
+	return res;
+}
